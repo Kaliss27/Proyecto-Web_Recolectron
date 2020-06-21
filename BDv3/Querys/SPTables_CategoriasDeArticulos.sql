f4bd8e15b1e18e6ab9cecc36385af76b54cc8857
Karen Peralta <karenlisspelu10@outlook.com>
2020-06-21 00:16:21 -0500

    Querys1.1

--- BDv3/Querys/SPTables_CategoriasDeArticulos.sql
+++ BDv3/Querys/SPTables_CategoriasDeArticulos.sql
@@ -3,43 +3,35 @@ delimiter //
 CREATE PROCEDURE fillTablesCategorias_de_Articulos()
 BEGIN
 /*Llenar tabla "Elementos Quimicos*/
-INSERT INTO elementos_quimicos(Elemento) VALUES
-("Arsénico"),
-("Cadmio"),
-("Litio"),
-("Mercurio"),
-("Selenio"),
-("Plomo"),
-("Gas refrigerante"),
-("Se saben tóxicos"),
-("No identificado");
+INSERT INTO elementos_quimicos(Elemento,Daños) VALUES
+("Arsénico","Una sustancia cancerígena."),
+("Cadmio","Metal pesado que puede provocar cáncer, debilidad en los huesos, daño a hígado y riñones, daño al sistema inmunitario, diarrea y hasta desórdenes psicológicos."),
+("Litio","El riñón, daña el sistema nervioso y genera problemas respiratorios."),
+("Mercurio","Afecta el sistema nervioso, irrita los ojos y provoca erupciones en la piel."),
+("Selenio","Puede ocasionar pelo quebradizo, hinchazón de la piel, dolores agudos y uñas deformadas."),
+("Plomo","Daña los riñones, el cerebro y todo el sistema nervioso, disminuye las habilidades de aprendizaje.");
 /*Llenar tabla "Categorias"*/
 INSERT INTO categorias_re(Categoria) VALUES
+("Electrodomésticos"),
+("Audio y Video"),
+("Iluminación"),
+("Hardware"),
+("Periféricos y Accesorios"),
 ("Componentes Electrónicos"),
-("Equipos de intercambio de temperatura"),
-("Grandes Equipos"),
-("Lámparas"),
-("Pantallas y monitores"),
-("Pequeños Aparatos"),
-("Pequeños artefactos de telec y TI");
+("Redes/IT"),
+("Otros");
 /*Llenar tabla Residuos Electronicos*/
 INSERT INTO residuos_electronicos(Descripcion,Categoria,Costo_Promedio_x_Unidad) VALUES
 ("Refrigerador",1,4500), 
 ("Congelador",1,9000), 
 ("Aire acondicionado",1,5000),
-("Bomba de calor",1,10000),
 ("Televisión",2,5000),
-("Computadora portátil",2,4000),
-("Tableta",2,1500),
+("Computadora portátil",4,4000),
 ("Lamparas LED",3,450),
-("Lavadora",4,4000),
-("Placa eléctrica",4,3500),
-("Lavavajilla",4,10000),
-("Aspiradoras",5,1000),
-("Horno de microondas",5,2000),
-("Grabadora de vídeo",5,1000),
-("Aparato de radio",5,500),
-("Teléfono celular",6,1000),
-("Dispositivo GPS",6,500),
-("Computadora de escritorio",6,64000);
+("Lavadora",1,4000),
+("Parillas eléctricas",1,3500),
+("Grabadora de vídeo",2,1000),
+("Radios",2,500),
+("Celulares",5,1000),
+("Laptops",3,64000);
 END
