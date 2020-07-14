082ba2244e8238c04f2d2c43c18f38969f6a3dc9
Alvaro <alvateca@gmail.com>
2020-07-13 22:12:58 -0500

    Update RegistroDe_Componentes.java

--- src/java/Modelo/RegistroDe_Componentes.java
+++ src/java/Modelo/RegistroDe_Componentes.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Modelo;
 
 /**
@@ -10,5 +6,51 @@ package Modelo;
  * @author Alvaro
  */
 public class RegistroDe_Componentes {
+    int id;
+    int fk_registrode;
+    int fk_componente;
+    int cantidad;
+
+    public RegistroDe_Componentes() {
+    }
+
+    public RegistroDe_Componentes(int fk_registrode, int fk_componente, int cantidad) {
+        this.fk_registrode = fk_registrode;
+        this.fk_componente = fk_componente;
+        this.cantidad = cantidad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getFk_registrode() {
+        return fk_registrode;
+    }
+
+    public void setFk_registrode(int fk_registrode) {
+        this.fk_registrode = fk_registrode;
+    }
+
+    public int getFk_componente() {
+        return fk_componente;
+    }
+
+    public void setFk_componente(int fk_componente) {
+        this.fk_componente = fk_componente;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
     
 }
